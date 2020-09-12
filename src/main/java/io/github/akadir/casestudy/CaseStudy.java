d3a5afbdac08471a3a5ce754b5f04788dd3ff9ef
kadir <akadirkkoc@gmail.com>
2020-09-12 10:09:30 +0300

    feat: add logback configuration

--- src/main/java/io/github/akadir/casestudy/CaseStudy.java
+++ src/main/java/io/github/akadir/casestudy/CaseStudy.java
@@ -1,8 +1,13 @@
 package io.github.akadir.casestudy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CaseStudy {
+    private static final Logger log = LoggerFactory.getLogger(CaseStudy.class);
+
     public static void main(String[] args) {
         // TODO run required flow 
-        System.out.println("Hello world!");
+        log.info("Hello world!");
     }
 }
