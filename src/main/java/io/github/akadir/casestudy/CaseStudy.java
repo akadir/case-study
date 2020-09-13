3895f883c3b55c4d81a51156326eaf53aca92fe8
kadir <akadirkkoc@gmail.com>
2020-09-13 13:57:52 +0300

    feat: update main method

--- src/main/java/io/github/akadir/casestudy/CaseStudy.java
+++ src/main/java/io/github/akadir/casestudy/CaseStudy.java
@@ -1,5 +1,7 @@
 package io.github.akadir.casestudy;
 
+import io.github.akadir.casestudy.delivery.strategy.FedexDelivery;
+import io.github.akadir.casestudy.delivery.strategy.UPSDelivery;
 import io.github.akadir.casestudy.discount.campaign.CategoryCampaign;
 import io.github.akadir.casestudy.discount.campaign.base.Campaign;
 import io.github.akadir.casestudy.discount.coupon.ConcreteCoupon;
@@ -9,8 +11,11 @@ import io.github.akadir.casestudy.product.model.Category;
 import io.github.akadir.casestudy.product.model.Product;
 import io.github.akadir.casestudy.shopping.service.ShoppingCartService;
 import io.github.akadir.casestudy.shopping.service.impl.ConcreteShoppingCartServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CaseStudy {
+    private static final Logger log = LoggerFactory.getLogger(CaseStudy.class);
 
     public static void main(String[] args) {
         Category food = new Category("food");
@@ -28,5 +33,16 @@ public class CaseStudy {
 
         shoppingCartService.applyCampaign(campaign);
         shoppingCartService.applyCoupon(coupon);
+
+        log.info("Shopping cart details without delivery strategy: {}", shoppingCartService.getShoppingCart());
+
+        shoppingCartService.getShoppingCart().setDeliveryStrategy(new FedexDelivery(1, 5));
+        log.info("Shopping cart details with FedexDelivery: {}", shoppingCartService.getShoppingCart());
+
+        log.info("Change delivery strategy to UPSDelivery");
+
+        shoppingCartService.getShoppingCart().setDeliveryStrategy(new UPSDelivery(2, 3));
+        log.info("Shopping cart details with UPSDelivery: {}", shoppingCartService.getShoppingCart());
+
     }
 }
